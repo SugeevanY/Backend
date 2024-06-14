0b37d6c004e1840bbc30c693998848541bb69f85
SugeevanY <acyogathassugeevan@gmail.com>
2024-06-14 08:16:24 +0530

    First Commit

---
+++ HandyHero/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HandyHero.Models
+{
+    public class Customer
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
