e7f4de4647d688de7e4c5cf7c040d7756844d686
ThiruPraveen997 <praveent.21@uom.lk>
2024-06-17 11:41:41 +0530

    Praveen Updated

---
+++ HandyHero/Controllers/CustomerController.cs
@@ -0,0 +1,157 @@
+using backend.DTO;
+using backend.Models;
+using backend.Services.Infrastructure;
+using backend.Services.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Claims;
+using System.Text;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        ICustomer _customer;
+        private readonly IConfiguration _config;
+        public CustomerController(ICustomer customer, IConfiguration config)
+        {
+            _customer = customer;
+            _config = config;
+        }
+
+        [HttpPost("create")]
+        public IActionResult createAccount([FromBody] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var result = _customer.SignUp(customer);
+                if (result)
+                {
+                    Console.WriteLine("registration success");
+                    return Ok(result);
+
+                }
+                else
+                {
+                    Console.WriteLine("registration failed");
+                    return BadRequest(result);
+                }
+            }
+            else
+            {
+                return BadRequest("ModelError");
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult login([FromBody] Customer customer)
+        {
+            var email = customer.Email;
+            var password = customer.Password;
+
+            var result = _customer.Login(email, password);
+            if (result)
+            {
+                var LoggedInCustomer = _customer.getCustomerByMail(email);
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+                var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                new Claim(ClaimTypes.Name, email),  // Email claim
+                new(ClaimTypes.NameIdentifier, LoggedInCustomer.Id.ToString()),  // Admin ID claim
+                new(ClaimTypes.GivenName, LoggedInCustomer.Name)  // Admin name claim
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(30),
+                    SigningCredentials = credentials
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return Ok(new { token = tokenHandler.WriteToken(token), Id = LoggedInCustomer.Id });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        [HttpPost("requestWork")]
+        public IActionResult requestWork([FromBody] Project project)
+        {
+            var result = _customer.createProject(project);
+            if (result)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("myProject")]
+        public IActionResult getMyProjects([FromQuery(Name = "id")] int Id)
+        {
+            var projects = _customer.getMyProject(Id);
+
+            if (projects == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(projects);
+            }
+        }
+
+        [HttpPost("complaint")]
+        public IActionResult makeComplaint([FromBody] ComplaintRequest complaint)
+        {
+            if (!ModelState.IsValid)
+            {
+                Console.WriteLine("Invalid model state");
+                return BadRequest("Error in model");
+            }
+
+            var customer = _customer.findCustomerById(complaint.complanantId);
+            if (customer == null)
+            {
+                Console.WriteLine("Customer not found");
+                Console.WriteLine(complaint.complanantId.GetType());
+                Console.WriteLine(complaint.complanantId.ToString());
+                return NotFound("Customer not found");
+            }
+
+            var complainantEmail = customer.Email;
+
+            Complaint complaint1 = new Complaint();
+            complaint1.Complainant = complainantEmail;
+            complaint1.Accused = complaint.accused;
+            complaint1.ComplaintMessage = complaint.complaint;
+            complaint1.TimeStamp = DateTime.Now;
+
+            var result = _customer.createComplaint(complaint1);
+            if (result)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong while creating complaint");
+                return BadRequest("Error in create complaint");
+            }
+        }
+
+
+    }
+}
